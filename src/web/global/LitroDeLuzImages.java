3f758b8f955cafa5e53b967466c4f72b7845f335
Juan Diego Espinosa Santos <juandiego@comtor.net>
2019-04-30 17:08:59 -0500

    * Nuevos íconos

---
+++ src/web/global/LitroDeLuzImages.java
@@ -0,0 +1,30 @@
+package web.global;
+
+/**
+ *
+ * @author juandiego@comtor.net
+ * @since
+ * @version Apr 30, 2019 
+ */
+public final class LitroDeLuzImages {
+
+    private static final String IMAGE_PREFIX = "framework/images/";
+    private static final String ICON_PREFIX = IMAGE_PREFIX + "icons/";
+    private static final String INDEX_ICON_PREFIX = ICON_PREFIX + "index/";
+    private static final String CONTROLLER_ICON_PREFIX = ICON_PREFIX + "controller/";
+    private static final String ACTION_ICON_PREFIX = ICON_PREFIX + "actions/";
+
+    public static final String LOGO_LOGIN = IMAGE_PREFIX + "litrodeluz-login-logo.png";
+    public static final String LOGO_APP = IMAGE_PREFIX + "litrodeluz-radius-logo.png";
+
+    public static final String USER_INDEX = INDEX_ICON_PREFIX + "user-index.png";
+    public static final String PROFILE_INDEX = INDEX_ICON_PREFIX + "profile-index.png";
+    public static final String ADVERTISING_INDEX = INDEX_ICON_PREFIX + "advertising-index.png";
+    public static final String HAPPYHOUR_INDEX = INDEX_ICON_PREFIX + "happyhour-index.png";
+    public static final String KIOSK_INDEX = INDEX_ICON_PREFIX + "kiosk-index.png";
+    public static final String MAP_INDEX = INDEX_ICON_PREFIX + "map-index.png";
+    public static final String RATE_INDEX = INDEX_ICON_PREFIX + "rate-index.png";
+    public static final String REPORT_INDEX = INDEX_ICON_PREFIX + "report-index.png";
+    public static final String SELLER_INDEX = INDEX_ICON_PREFIX + "seller-index.png";
+    public static final String HOTSPOT_INDEX = INDEX_ICON_PREFIX + "hotspot-index.png";
+}
