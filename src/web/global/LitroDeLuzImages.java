42f33bc0b930e866be15bf09e9a480e57f34571e
Juan Diego Espinosa Santos <juandiego@comtor.net>
2019-05-02 16:36:10 -0500

    Estilos, logos e íconos Litro de Luz.

--- src/web/global/LitroDeLuzImages.java
+++ src/web/global/LitroDeLuzImages.java
@@ -4,7 +4,7 @@ package web.global;
  *
  * @author juandiego@comtor.net
  * @since
- * @version Apr 30, 2019 
+ * @version Apr 30, 2019
  */
 public final class LitroDeLuzImages {
 
@@ -14,8 +14,10 @@ public final class LitroDeLuzImages {
     private static final String CONTROLLER_ICON_PREFIX = ICON_PREFIX + "controller/";
     private static final String ACTION_ICON_PREFIX = ICON_PREFIX + "actions/";
 
-    public static final String LOGO_LOGIN = IMAGE_PREFIX + "litrodeluz-login-logo.png";
-    public static final String LOGO_APP = IMAGE_PREFIX + "litrodeluz-radius-logo.png";
+    public static final String RADIUS_LOGO_LOGIN = IMAGE_PREFIX + "litrodeluz-radius-login-logo.png";
+    public static final String MEDIA_MANAGER_LOGO_LOGIN = IMAGE_PREFIX + "litrodeluz-mediamanager-login-logo.png";
+    public static final String RADIUS_LOGO_APP = IMAGE_PREFIX + "litrodeluz-radius-logo.png";
+    public static final String MEDIA_MANAGER_LOGO_APP = IMAGE_PREFIX + "litrodeluz-mediamanager-logo.png";
 
     public static final String USER_INDEX = INDEX_ICON_PREFIX + "user-index.png";
     public static final String PROFILE_INDEX = INDEX_ICON_PREFIX + "profile-index.png";
@@ -27,4 +29,23 @@ public final class LitroDeLuzImages {
     public static final String REPORT_INDEX = INDEX_ICON_PREFIX + "report-index.png";
     public static final String SELLER_INDEX = INDEX_ICON_PREFIX + "seller-index.png";
     public static final String HOTSPOT_INDEX = INDEX_ICON_PREFIX + "hotspot-index.png";
+    
+    public static final String MOVIES_INDEX = INDEX_ICON_PREFIX + "movies-index.png";
+    public static final String MUSIC_INDEX = INDEX_ICON_PREFIX + "music-index.png";
+    public static final String DUMP_INDEX = INDEX_ICON_PREFIX + "dump-index.png";
+
+    public static final String USER_CONTROLLER = CONTROLLER_ICON_PREFIX + "user-controller.png";
+    public static final String PROFILE_CONTROLLER = CONTROLLER_ICON_PREFIX + "profile-controller.png";
+    public static final String ADVERTISING_CONTROLLER = CONTROLLER_ICON_PREFIX + "advertising-controller.png";
+    public static final String HAPPYHOUR_CONTROLLER = CONTROLLER_ICON_PREFIX + "happyhour-controller.png";
+    public static final String KIOSK_CONTROLLER = CONTROLLER_ICON_PREFIX + "kiosk-controller.png";
+    public static final String MAP_CONTROLLER = CONTROLLER_ICON_PREFIX + "map-controller.png";
+    public static final String RATE_CONTROLLER = CONTROLLER_ICON_PREFIX + "rate-controller.png";
+    public static final String REPORT_CONTROLLER = CONTROLLER_ICON_PREFIX + "report-controller.png";
+    public static final String SELLER_CONTROLLER = CONTROLLER_ICON_PREFIX + "seller-controller.png";
+    public static final String HOTSPOT_CONTROLLER = CONTROLLER_ICON_PREFIX + "hotspot-controller.png";
+
+    public static final String FILM_GENRE_CONTROLLER = CONTROLLER_ICON_PREFIX + "film_genre_controller.png";
+    public static final String MUSIC_GENRE_CONTROLLER = CONTROLLER_ICON_PREFIX + "music_genre_controller.png";
+    public static final String DUMP_CONTROLLER = CONTROLLER_ICON_PREFIX + "dump-controller.png";
 }
